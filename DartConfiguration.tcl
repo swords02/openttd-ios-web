6322183d0d85daf3fbdc16af3799bf3592425907
swords02 <owengerke@icloud.com>
2025-10-20 00:51:17 -0500

    Add files via upload

--- DartConfiguration.tcl
+++ DartConfiguration.tcl
@@ -11,7 +11,7 @@ BuildDirectory: /c/Users/oweng/OneDrive/Desktop/OpenTTD-master/build
 CostDataFile: 
 
 # Site is something like machine.domain, i.e. pragmatic.crd
-Site: 5b5c6ca99183
+Site: 095979488eea
 
 # Build name is osname-revision-compiler, i.e. Linux-2.4.2-2smp-c++
 BuildName: Emscripten-em++
